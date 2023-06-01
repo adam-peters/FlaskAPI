75d9a278950d27a0c2c797a1465c405abcb3f17d
Adam Peters <mradampeters1@gmail.com>
2023-06-01 11:33:16 -0700

    Added templates and static folders

--- hello.py
+++ hello.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask import url_for
+from flask import request
+from flask import render_template
 from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/user/<username>")
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/user/<username>')
 def show_user_profile(username):
     return f'User {escape(username)}'
 
@@ -22,3 +29,57 @@ def projects():
 @app.route('/about')
 def about():
     return 'The about page'
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route("/me")
+def me_api():
+    user = get_current_user()
+    return {
+        "username": user.username,
+        "theme": user.theme,
+        "image": url_for("user_image", filename=user.image),
+    }
+
+@app.route("/users")
+def users_api():
+    users = get_all_users()
+    return [user.to_json() for user in users]
+
+# URL Building
+# @app.route('/login')
+# def login():
+#     return 'login'
+
+# @app.route('/user/<username>')
+# def profile(username):
+#     return f'{username}\'s profile'
+
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('login'))
+#     print(url_for('login', next='/'))
+#     print(url_for('profile', username='John Doe'))
+
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         return do_the_login()
+#     else:
+#         return show_the_login_form()
+
+# Can also seperate above with
+# @app.get('/login')
+# def login_get():
+#     return show_the_login_form()
+
+# @app.post('/login')
+# def login_post():
+#     return do_the_login()
+
+# url_for('static', filename='style.css')
