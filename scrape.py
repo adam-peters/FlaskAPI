328c6f4473a939c4238f3d5af04c74077be0b920
Adam Peters <mradampeters1@gmail.com>
2023-06-26 23:09:54 -0700

    Added Scraping from WalkScore Website

--- scrape.py
+++ scrape.py
@@ -1,9 +1,12 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open('./templates/hello.html', 'r') as html_file:
-    content = html_file.read();
-   
-    soup = BeautifulSoup(content, 'lxml')
-    tags = soup.find_all('h2') # soup.find_all('div', class_='card')
-    for tag in tags:
-        print(tag.text)
+html_text = requests.get('https://www.walkscore.com/cities-and-neighborhoods/').text
+soup = BeautifulSoup(html_text, 'lxml')
+
+block = soup.find_all('div', class_='mag-block')[1]
+
+titles = block.find_all('h2')
+
+for title in titles:
+    print(title.text)
