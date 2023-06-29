2f31e836bacd75a7ac6e12d1991862587d08f25a
Adam Peters <mradampeters1@gmail.com>
2023-06-28 23:44:43 -0700

    Now returns Walk/Transit/Bike Score of Inputted City

--- scrape.py
+++ scrape.py
@@ -1,13 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 
-# city = input("Name a City in California: ")
-city = "Riverside"
+city = input("Name a City in California: ")
+# city = "Riverside"
 
 html_text = requests.get('https://www.walkscore.com/CA/' + city).text
 soup = BeautifulSoup(html_text, 'lxml')
 
-scores = soup.find_all('img')
+score_card = soup.find('div', class_='score-info-link')
+scores = score_card.find_all('img')
 
 for score in scores:
-    print(score)
+    print(score['alt'])
+
